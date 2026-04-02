25b7f0bef94c70d0419ac534a414fb7e790a0e95
Vijay Bansode <bansode.vijay1996@gmail.com>
2026-04-02 10:36:16 +0530

    login and refreshtoken

--- src/auth/firebase.ts
+++ src/auth/firebase.ts
@@ -8,15 +8,15 @@ import * as admin from 'firebase-admin';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyA_rBak5Zb7rlQmBK70y4rHOI-fnDlvhp0',
-  authDomain: 'mywallet-afcbb.firebaseapp.com',
-  databaseURL: 'https://mywallet-afcbb-default-rtdb.firebaseio.com',
-  projectId: 'mywallet-afcbb',
-  storageBucket: 'mywallet-afcbb.firebasestorage.app',
-  messagingSenderId: '261378138156',
-  appId: '1:261378138156:web:3b20797308f7abfd2ab189',
-  measurementId: 'G-RRH82BBVYG',
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
